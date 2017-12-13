0b88be75e62d1f272058a222ee67a9bd70c43f2b
raymondborkowski <rborkows@umich.edu>
2017-12-13 00:20:17 -0500

    RB: Add linter

--- bot.js
+++ bot.js
@@ -1,35 +1,34 @@
-/*global require, module, console */
-const botBuilder = require('claudia-bot-builder'),
-	  getIntentName = function (alexaPayload) {
-		  'use strict';
-		  return alexaPayload &&
-			  alexaPayload.request &&
-			  alexaPayload.request.type === 'IntentRequest' &&
-			  alexaPayload.request.intent &&
-			  alexaPayload.request.intent.name;
-	  }; 
+/* global require, module, console */
+import AlexaMessageBuilder from 'alexa-message-builder';
+import botBuilder from 'claudia-bot-builder';
+
+function getIntentName(alexaPayload){
+    return alexaPayload &&
+      alexaPayload.request &&
+      alexaPayload.request.type === 'IntentRequest' &&
+      alexaPayload.request.intent &&
+      alexaPayload.request.intent.name;
+}
 const api = botBuilder(
-	function (message, originalRequest) {
-		'use strict';
-		console.log(originalRequest.body.request.intent);
-		console.log(getIntentName(originalRequest.body));
-		if (originalRequest.body.request.intent.slots.Food) {
-			 return 'Ain\'t nobody got time to make ' + originalRequest.body.request.intent.slots.Food.value;
-		} else if (getIntentName(originalRequest.body) === 'ExitApp'){
-			return {
-				response: {
-					outputSpeech: {
-						type: 'PlainText',
-						text: 'Bye from Spelling Bee!'
-					},
-					shouldEndSession: true
-				}
-			};
-		} else {
-			return {};
-		}
-	},
-	{ platforms: ['alexa'] }
+    (message, originalRequest) => {
+        console.log(originalRequest.body.request.intent);
+        console.log(getIntentName(originalRequest.body));
+        if (originalRequest.body.request.intent.slots.Food) {
+            return `Ain't nobody got time to make ${originalRequest.body.request.intent.slots.Food.value}`;
+        } else if (getIntentName(originalRequest.body) === 'ExitApp') {
+            return {
+                response: {
+                    outputSpeech: {
+                        type: 'PlainText',
+                        text: 'Bye from Spelling Bee!',
+                    },
+                    shouldEndSession: true,
+                },
+            };
+        }
+        return {};
+    },
+    { platforms: ['alexa'] },
 );
 
 module.exports = api;
